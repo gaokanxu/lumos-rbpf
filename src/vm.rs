08405451fb7fbade5e952d80abf3347febeb5527
gaokanxu <gaokanxu@gmail.com>
2024-07-25 23:00:36 +0800

    rename solana to lumos

--- src/vm.rs
+++ src/vm.rs
@@ -1,11 +1,6 @@
 #![allow(clippy::arithmetic_side_effects)]
 // Derived from uBPF <https://github.com/iovisor/ubpf>
-// Copyright 2015 Big Switch Networks, Inc
-//      (uBPF: VM architecture, parts of the interpreter, originally in C)
-// Copyright 2016 6WIND S.A. <quentin.monnet@6wind.com>
-//      (Translation to Rust, MetaBuff/multiple classes addition, hashmaps for syscalls)
-// Copyright 2020 Solana Maintainers <maintainers@solana.com>
-//
+
 // Licensed under the Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0> or
 // the MIT license <http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
